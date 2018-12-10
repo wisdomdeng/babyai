880b2c49f3e8a19fcede2027c4d3e78bd6a9c910
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-12-10 16:27:14 -0500

    Implemented reinforce algorithm

--- ga_rl.py
+++ ga_rl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import copy
 import math
 from functools import reduce
 import operator
@@ -92,15 +93,18 @@ print('num actions:', num_actions)
 max_steps = env.max_steps
 print('max episode steps:', max_steps)
 
-max_episodes = 10000
+max_episodes = 8192
 num_episodes = 0
+eps_idx = 0
 
 # Done indicates that we become done after the current step
-obs = np.zeros(shape=(max_episodes, max_steps, 147))
-action = np.zeros(shape=(max_episodes, max_steps, 1), dtype=np.long)
-reward = np.zeros(shape=(max_episodes, max_steps, 1))
+obss = np.zeros(shape=(max_episodes, max_steps, 147))
+actions = np.zeros(shape=(max_episodes, max_steps, 1), dtype=np.long)
+rewards = np.zeros(shape=(max_episodes, max_steps, 1))
 active = np.zeros(shape=(max_episodes, max_steps, 1))
 
+# Environment to collect experience from
+env = gym.make('BabyAI-GoToRedBall-v0')
 
 
 
@@ -134,14 +138,157 @@ def evaluate(model, seed=0, num_episodes=100):
 
     return num_success / num_episodes
 
+
+
+
+def gen_experience(env, model, num_rollouts=8):
+    global eps_idx
+    global num_episodes
+
+    for i in range(num_rollouts):
+        #print(eps_idx)
+
+        # Reset active flags for episode
+        active[eps_idx] = 0
+
+        obs = env.reset()
+
+        memory = Variable(torch.zeros([1, 128])).cuda()
+
+        for step_idx in range(max_steps):
+            active[eps_idx, step_idx] = 1
+
+            obs = obs['image']
+            obss[eps_idx, step_idx] = obs.reshape((147,))
+
+            obs = make_var(obs).unsqueeze(0)
+            dist, memory = model.predict_action(obs, memory)
+            action = dist.sample()
+
+            actions[eps_idx, step_idx] = action
+
+            obs, reward, done, info = env.step(action)
+
+            if done:
+                break
+
+        for i in reversed(range(step_idx+1)):
+            rewards[eps_idx, i] = reward
+            reward *= 0.99
+
+        num_episodes = max(num_episodes, eps_idx+1)
+        eps_idx = (eps_idx + 1) % max_episodes
+
+
+
+
+def reinforce(optimizer, model, batch_size=256):
+    batch_size = min(batch_size, num_episodes)
+
+    # Select a valid episode index in function of the batch size
+    eps_idx = np.random.randint(0, num_episodes - batch_size + 1)
+
+    # Get the observations, actions and done flags for this batch
+    obs_batch = obss[eps_idx:(eps_idx+batch_size)]
+    act_batch = actions[eps_idx:(eps_idx+batch_size)]
+    rew_batch = rewards[eps_idx:(eps_idx+batch_size)]
+    active_batch = active[eps_idx:(eps_idx+batch_size)]
+
+    obs_batch = make_var(obs_batch)
+    act_batch = make_var(act_batch)
+    rew_batch = make_var(rew_batch)
+    active_batch = make_var(active_batch)
+
+    memory = Variable(torch.zeros([batch_size, 128])).cuda()
+
+    policy_loss = 0
+
+    mean_rew = rew_batch.mean()
+
+    # For each step
+    # We will iterate until the max demo len (or until all demos are done)
+    for step_idx in range(max_steps):
+        active_step = active_batch[:, step_idx, :]
+
+        if active_step.sum().item() == 0:
+            break
+
+        obs_step = obs_batch[:, step_idx, :]
+        act_step = act_batch[:, step_idx, :]
+        rew_step = rew_batch[:, step_idx, :]
+
+        dist, memory = model.predict_action(obs_step, memory)
+
+        #print(act_step.size())
+
+        log_prob = dist.log_prob(act_step.squeeze(1))
+
+        #print(log_prob.size())
+        #print(rew_step.size())
+        #print(active_step.size())
+
+        #rew_step = rew_step - mean_rew
+        policy_loss += (-log_prob * rew_step * active_step).sum()
+
+    #print(policy_loss)
+
+    optimizer.zero_grad()
+    policy_loss.backward()
+    optimizer.step()
+
+
+
+
+
+
+
+
+
+
+
+model = Model(num_actions)
+model.cuda()
+
+optimizer = optim.SGD(model.parameters(), lr=1e-4)
+
+best_score = 0
+best_model = model
+
+
+
+
+
+for i in range(10000):
+    #model = copy.deepcopy(best_model)
+
+    print('  gen experience')
+    gen_experience(env, model)
+
+    print('  reinforce')
+    reinforce(optimizer, model, batch_size=16)
+
+
+    if i % 20 == 0:
+        print('evaluate')
+        s = evaluate(model)
+        print('#{}: {:.3f}'.format(i+1, s))
+
+    #if s > best_score:
+    #    print('new best score: {:.3f}'.format(s))
+    #    best_model = copy.deepcopy(model)
+    #    best_score = s
+
+
+
+
 """
 for i in range(100):
     model = Model(num_actions)
     model.cuda()
-
     s = evaluate(model)
 
     print('#{}: {:.3f}'.format(i+1, s))
+
 """
 
 # TODO; start with 10 random models, evaluate them
